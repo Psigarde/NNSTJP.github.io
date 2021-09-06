d56f5ce63968810a051c81a60451ada5ee4efef4
Sigurd Sørbotten <sigurdaksjen@gmail.com>
2021-09-06 18:58:28 +0200

    changed to better indicate as being scripts

--- Idola/UpdateCharacters.py
+++ Idola/UpdateCharacters.py
@@ -40,25 +40,29 @@ class MyJSONEncoder(json.JSONEncoder):
         else:
             return json.dumps(o)
 
-with open("Idola/CharacterswithLBDB.json") as file:
-    cl = json.load(file)
-file.close()
+def main():
+    with open("Idola/CharacterswithLBDB.json") as file:
+        cl = json.load(file)
+    file.close()
 
-with open("Idola/UpdateTheseCharacters.json") as file:
-    updateCharacters = json.load(file)
-file.close()
+    with open("Idola/UpdateTheseCharacters.json") as file:
+        updateCharacters = json.load(file)
+    file.close()
 
-for updateID in updateCharacters:
-    if updateID in cl:
-        for updates in updateCharacters[updateID]:
-            cl[updateID][updates] = updateCharacters[updateID][updates]
-    else:
-        cl[updateID] ={}
-        cl[updateID] |= updateCharacters[updateID]
-finishedList = json.dumps(cl, indent=4, cls=MyJSONEncoder)
-with open("Idola/CharacterswithLBDB.json", "w") as json_file:
-    json_file.write(finishedList)
-    json_file.write("\n")
-json_file.close()
+    for updateID in updateCharacters:
+        if updateID in cl:
+            for updates in updateCharacters[updateID]:
+                cl[updateID][updates] = updateCharacters[updateID][updates]
+        else:
+            cl[updateID] ={}
+            cl[updateID] |= updateCharacters[updateID]
+    finishedList = json.dumps(cl, indent=4, cls=MyJSONEncoder)
+    with open("Idola/CharacterswithLBDB.json", "w") as json_file:
+        json_file.write(finishedList)
+        json_file.write("\n")
+    json_file.close()
 
-exec(open('Idola/FixThisShit.py').read())
+    exec(open('Idola/UpdateJSFiles.py').read())
+
+if __name__ == '__main__':
+    main()
